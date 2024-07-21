06a2d47862fc844f87b3baa022e124cca9853741
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2024-07-21 00:57:32 -0500

    Commit

--- embed.py
+++ embed.py
@@ -83,7 +83,7 @@ def main():
 
     if test_text_embedding:
         model_name = "clip-ViT-B-32"
-        text_to_embed = "What is Pgvector Embedding?"
+        text_to_embed = "How do we create embedding for PostgreSQL?"
         cemd.create_text_embedding(text_to_embed=text_to_embed, model_name=model_name, show=show)
 
     if test_image_embedding:
