d850fb08e327071ee9643cd8c141c662e4749b86
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2024-07-21 05:46:46 -0500

    Commit

--- embed-ddl.sql
+++ embed-ddl.sql
@@ -5,9 +5,7 @@ CREATE TABLE IF NOT EXISTS public.images (
   image_label VARCHAR(10) NOT NULL,
   image_embedding VECTOR(128) NOT NULL
 );
-
 CREATE INDEX IF NOT EXISTS image_embedding_idx ON  public.images USING hnsw (embedding vector_cosine_ops);
-
 
 -- chat table
 CREATE TABLE IF NOT EXISTS public.chat (
@@ -15,5 +13,4 @@ CREATE TABLE IF NOT EXISTS public.chat (
   createdon TIMESTAMP NOT NULL, 
   chat_embedding VECTOR(32) NOT NULL
 );
-
 CREATE INDEX IF NOT EXISTS chat_embedding_idx ON  public.chat USING  hnsw (embedding vector_cosine_ops);
