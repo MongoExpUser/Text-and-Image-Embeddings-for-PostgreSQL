799ff3a484abb5231b362ad0e451a4c959dd8895
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2024-07-21 06:01:27 -0500

    Commit

--- embed-ddl.sql
+++ embed-ddl.sql
@@ -1,16 +1,21 @@
+-- schema
+CREATE SCHEMA IF NOT EXISTS vsearch AUTHORIZATION postgres;
+
 -- image table
-CREATE TABLE IF NOT EXISTS public.images (
+DROP TABLE IF EXISTS vsearch.images CASCADE;
+CREATE TABLE IF NOT EXISTS vsearch.images (
   id INTEGER PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY,
   image_filename VARCHAR(15) NOT NULL,
   image_label VARCHAR(10) NOT NULL,
   image_embedding VECTOR(128) NOT NULL
 );
-CREATE INDEX IF NOT EXISTS image_embedding_idx ON  public.images USING hnsw (embedding vector_cosine_ops);
+CREATE INDEX IF NOT EXISTS image_embedding_idx ON  vsearch.images USING hnsw (image_embedding vector_cosine_ops);
 
--- chat table
-CREATE TABLE IF NOT EXISTS public.chat (
+-- text table
+DROP TABLE IF EXISTS vsearch.text CASCADE;
+CREATE TABLE IF NOT EXISTS vsearch.text (
   id INTEGER PRIMARY KEY NOT NULL GENERATED ALWAYS  AS IDENTITY,
   createdon TIMESTAMP NOT NULL, 
-  chat_embedding VECTOR(32) NOT NULL
+  text_embedding VECTOR(32) NOT NULL
 );
-CREATE INDEX IF NOT EXISTS chat_embedding_idx ON  public.chat USING  hnsw (embedding vector_cosine_ops);
+CREATE INDEX IF NOT EXISTS text_embedding_idx ON vsearch.text USING  hnsw (text_embedding vector_cosine_ops);
